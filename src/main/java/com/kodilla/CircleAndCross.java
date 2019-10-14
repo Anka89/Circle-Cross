7d66e7821668e3f41cadcfb2101813e05f2face4
Ania <zegota.anka@gmail.com>
2019-10-14 17:41:33 +0200

    Corrects for background

--- src/main/java/com/kodilla/CircleAndCross.java
+++ src/main/java/com/kodilla/CircleAndCross.java
@@ -1,17 +1,18 @@
 package com.kodilla;
 
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
     public class CircleAndCross extends Application {
 
-        private Image imageback = new Image("file:resources/file/BG.png");
+        private Image imageback = new Image("file/Background.jpg");
+        private Image hash = new Image( "file/Hash.xcf" );
+        private FlowPane grids = new FlowPane( Orientation.HORIZONTAL);
 
         public static void main(String[] args) {
             launch(args);
@@ -20,13 +21,13 @@ import javafx.stage.Stage;
         @Override
         public void start(Stage primaryStage) throws Exception {
             BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
-            BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+            BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
             Background background = new Background(backgroundImage);
 
             GridPane grid = new GridPane();
             grid.setBackground(background);
 
-            Scene scene = new Scene(grid, 800, 600, Color.WHITE);
+            Scene scene = new Scene(grid, 1600, 1000, Color.WHITE);
 
             primaryStage.setTitle("Circle and Cross");
             primaryStage.setScene(scene);
