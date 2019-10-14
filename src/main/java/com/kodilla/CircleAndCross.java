ff6b331b6de1d72ec6cbf6dfa24761d9287cbca7
Ania <zegota.anka@gmail.com>
2019-10-14 19:13:37 +0200

    Try to add image without grid

--- src/main/java/com/kodilla/CircleAndCross.java
+++ src/main/java/com/kodilla/CircleAndCross.java
@@ -1,36 +1,40 @@
 package com.kodilla;
 
 import javafx.application.Application;
-import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-    public class CircleAndCross extends Application {
+import javax.swing.*;
 
-        private Image imageback = new Image("file/Background.jpg");
-        private Image hash = new Image( "file/Hash.xcf" );
-        private FlowPane grids = new FlowPane( Orientation.HORIZONTAL);
+public class CircleAndCross extends Application {
 
-        public static void main(String[] args) {
-            launch(args);
-        }
+    private Image imageback = new Image("file/Background.jpg");
+    private Image hash = new Image("file/Hash.png");
 
-        @Override
-        public void start(Stage primaryStage) throws Exception {
-            BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
-            BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
-            Background background = new Background(backgroundImage);
 
-            GridPane grid = new GridPane();
-            grid.setBackground(background);
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-            Scene scene = new Scene(grid, 1600, 1000, Color.WHITE);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, true);
+        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
 
-            primaryStage.setTitle("Circle and Cross");
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        }
+        GridPane grid = new GridPane();
+        grid.setBackground(background);
+        Scene scene = new Scene(grid, 1600, 900, Color.WHITE);
+
+        primaryStage.setTitle("BlackJack");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        ImageView image = new ImageView();
+        image.setImage(hash);
+
+    }
 }
